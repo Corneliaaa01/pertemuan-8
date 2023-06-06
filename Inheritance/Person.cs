7aea8d135a90413238975b451bc7dc0a5486a049
Cornelia <cornelia@students.amikom.ac.id>
2023-06-06 08:51:40 +0700

    first commit

---
+++ Inheritance/Person.cs
@@ -0,0 +1,41 @@
+﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void InfoPerson()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+
+    class Karyawan : Person
+    {
+        public string JobTitle { get; set; }
+        public Karyawan(string name, int age, string jobTitle) : base(name, age)
+        {
+            JobTitle = jobTitle;
+        }
+
+        public void InfoPerson()
+        {
+            base.InfoPerson();
+            Console.WriteLine("Job Title: " + JobTitle);
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Abdul Mizwar", 24);
+            person1.InfoPerson();
+
+            Karyawan karyawan1 = new Karyawan("Putri", 24, "Dosen");
+            karyawan1.InfoPerson();
+
+            Console.ReadKey();
+        }
+    }
+}
